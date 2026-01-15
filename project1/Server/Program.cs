c287c0e12ce42e4081f2d9846e259a258935a41d
jasmin <jasmin.software9@gmail.com>
2026-01-14 22:12:29 -0800

    Server creation

---
+++ project1/Server/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using Azure;
+using Azure.AI.OpenAI;
+using Microsoft.Agents.AI;
+using Microsoft.Agents.AI.Hosting.AGUI.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OpenAI.Chat;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHttpClient().AddLogging();
+builder.Services.AddAGUI();
+
+WebApplication app = builder.Build();
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .Build();
+            
+
+Console.WriteLine($"Base path: {AppContext.BaseDirectory}");
+Console.WriteLine($"GetCurrentDirectory path: {Directory.GetCurrentDirectory()}");
+Console.WriteLine($"Token loaded: {config["GitHub:Token"] is not null}");
+
+string? apiKey = config["GitHub:Token"];
+string? endpoint = config["GitHub:ApiEndpoint"] ?? "https://models.github.ai/inference";
+string? deploymentName = config["GitHub:Model"] ?? "openai/gpt-4o-mini";
+
+    
+// Create AI agent
+AIAgent agent = new AzureOpenAIClient(
+    new Uri(endpoint),
+    new AzureKeyCredential(apiKey!))
+    .GetChatClient(deploymentName)
+    .CreateAIAgent(instructions: "You're a wise motivational speaker.", name: "Socrates");
+
+// Map the AG-UI agent endpoint
+app.MapAGUI("/", agent);
+
+await app.RunAsync();
+
+// Create simple tools
