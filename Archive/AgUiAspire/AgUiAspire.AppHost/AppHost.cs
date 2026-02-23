9ce1a55ee435e6c5671c96465676ffeefe678648
jasmin <jasmin.software9@gmail.com>
2026-02-23 15:19:56 -0800

    Removed Aspire version

--- Archive/AgUiAspire/AgUiAspire.AppHost/AppHost.cs
+++
@@ -1,7 +0,0 @@
-var builder = DistributedApplication.CreateBuilder(args);
-
-var server = builder.AddProject<Projects.Server>("server");
-
-var blazor = builder.AddProject<Projects.Blazor_Client>("blazor");
-
-builder.Build().Run();
